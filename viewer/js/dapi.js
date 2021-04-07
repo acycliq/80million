49abc104fc85c771cc095d69fff1b5ead915a138
Dimitris <dimitris.nicoloutsopoulos@gmail.com>
2021-04-07 08:48:58 +0100

    clean up

--- viewer/js/dapi.js
+++ viewer/js/dapi.js
@@ -2,7 +2,7 @@ function dapi(cfg) {
     console.log('Doing Dapi plot');
 
     var img = cfg.imageSize,
-        map_tiles = cfg.tiles,
+        map_tiles = cfg.map_tiles,
         roi = cfg.roi;
 
     var a = img[0] / (roi.x1 - roi.x0),
@@ -22,9 +22,6 @@ function dapi(cfg) {
     var minZoom = 0,
         maxZoom = 8;
     var dapiLayer = L.tileLayer(map_tiles, {minZoom: minZoom, maxZoom: maxZoom});
-    var Tph2Layer = L.tileLayer('./src/pyramid/Tph2/{z}/{y}/{x}.png', {minZoom: minZoom, maxZoom: maxZoom});
-    var Slc1a2Layer = L.tileLayer('./src/pyramid/Slc1a2/{z}/{y}/{x}.png', {minZoom: minZoom, maxZoom: maxZoom});
-    var Nos1Layer = L.tileLayer('./src/pyramid/Nos1/{z}/{y}/{x}.png', {minZoom: minZoom, maxZoom: maxZoom});
 
     map = L.map('mymap', {
         layers: [dapiLayer],
@@ -32,25 +29,12 @@ function dapi(cfg) {
         attributionControl: false,
     }).setView([img[1], img[0] / 2], 2);
 
-    map.addLayer(Tph2Layer);
-    map.addLayer(Slc1a2Layer);
-
-    var baseLayers = {
-        "Dapi image": dapiLayer,
-    };
-
-    var overlayMaps = {
-        "Tph2": Tph2Layer,
-        "Slc1a2": Slc1a2Layer,
-        "Nos1": Nos1Layer,
-
-    };
-
-    L.control.layers(baseLayers, overlayMaps, {collapsed: false}).addTo(map);
 
     var dapiData = {};
     dapiData.map = map;
     dapiData.t = t;
+    dapiData.minZoom = minZoom;
+    dapiData.maxZoom = maxZoom;
 
     return dapiData
 }
@@ -59,6 +43,47 @@ function dapi(cfg) {
 function dapiChart(config) {
 
     dapiConfig = dapi(config);
-    var map = dapiConfig.map;
+    var map = dapiConfig.map,
+        minZoom = dapiConfig.minZoom,
+        maxZoom = dapiConfig.maxZoom;
+
+    var gene_tiles = [],
+        gene_names;
+
+
+    const repo = (gene) => {
+        // return "https://raw.githubusercontent.com/acycliq/gene_tiles/master/" + gene + "/{z}/{y}/{x}.png"
+        return "./src/pyramid/" + gene + "/{z}/{y}/{x}.png"
+    };
+
+    gene_names = glyphSettings().map(d => d.gene);
+    gene_names.forEach(gene => {
+        var d = {};
+        d.gene = gene;
+        d.tileLayer = L.tileLayer(repo(gene),  {minZoom: minZoom, maxZoom: maxZoom});
+        gene_tiles.push(d)
+    });
+
+    var Tph2Layer = L.tileLayer('./src/pyramid/Tph2/{z}/{y}/{x}.png', {minZoom: minZoom, maxZoom: maxZoom});
+    var Slc1a2Layer = L.tileLayer('./src/pyramid/Slc1a2/{z}/{y}/{x}.png', {minZoom: minZoom, maxZoom: maxZoom});
+    var Nos1Layer = L.tileLayer('./src/pyramid/Nos1/{z}/{y}/{x}.png', {minZoom: minZoom, maxZoom: maxZoom});
+
+    map.addLayer(Tph2Layer);
+    map.addLayer(Slc1a2Layer);
+
+    // var baseLayers = {
+    //     "Dapi image": dapiLayer,
+    // };
+
+    // var overlayMaps = {
+    //     "Tph2": Tph2Layer,
+    //     "Slc1a2": Slc1a2Layer,
+    //     "Nos1": Nos1Layer,
+    //
+    // };
+
+    var overlayMaps = Object.assign({}, ...gene_tiles.map((x) => ({[x.gene]: x.tileLayer})));
+    L.control.layers({}, overlayMaps, {collapsed: false}).addTo(map);
+
 }
 
