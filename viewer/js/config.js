4a6d26eb167a34aef526cec1ce79c9772407d859
Dimitris <dimitris.nicoloutsopoulos@gmail.com>
2021-04-07 08:48:25 +0100

    minor edit

--- viewer/js/config.js
+++ viewer/js/config.js
@@ -2,7 +2,7 @@ function config() {
     return {
             roi: {"x0": -5071.627079999999, "x1": 4622.530679999998, "y0": -6427.59084, "y1": 221.184 },
             imageSize: [262144, 179792],
-            tiles: 'https://raw.githubusercontent.com/acycliq/VS6_MsBrain_A3_maptiles/master/maptiles/262144px/z3/{z}/{y}/{x}.jpg'
+            map_tiles: 'https://raw.githubusercontent.com/acycliq/VS6_MsBrain_A3_maptiles/master/maptiles/262144px/z3/{z}/{y}/{x}.jpg'
     }
 }
 
